a3ac9b5a4f8fcc5a85b79ae0283dd5c05c74784f
Fabien Rassinier <frassinier@talend.com>
2021-06-13 17:10:46 +0200

    Updates

---
+++ _next/static/tfbuiVpn8aSbGc7gc706D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-780c819b2d3c4171648b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bbbdaae26be4d4d62c49.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
